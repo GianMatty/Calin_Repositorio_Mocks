8bc67369f1f07e377aa2e86bef594505d27a3d81
GianMatty <matiasmauriciogiancarlo@gmail.com>
2022-08-01 13:16:49 -0500

    updated

--- src/controller/serverController.js
+++ src/controller/serverController.js
@@ -9,7 +9,7 @@ const XLSX = require("xlsx");
 
 
 const parse = () => {
-    const excelData = XLSX.readFile(path.join(__dirname, "../dat/tienda360.xlsx"));
+    const excelData = XLSX.readFile(path.join(__dirname, "../data/tienda360.xlsx"));
     const parseExcelData = Object.keys(excelData.Sheets).map((name) => ({
         name,
         data: XLSX.utils.sheet_to_json(excelData.Sheets[name]),
