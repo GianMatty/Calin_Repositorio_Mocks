d9ce840f7b1631ea3d102774da118bd8ed9e3e59
GianMatty <matiasmauriciogiancarlo@gmail.com>
2022-08-01 13:24:27 -0500

    updated

--- src/controller/serverController.js
+++ src/controller/serverController.js
@@ -9,7 +9,7 @@ const XLSX = require("xlsx");
 
 
 const parse = () => {
-    const excelData = XLSX.readFile(path.join(__dirname, "../data/tienda360.xlsx"));
+    const excelData = XLSX.readFile(__dirname + "/tienda360.xlsx");
     const parseExcelData = Object.keys(excelData.Sheets).map((name) => ({
         name,
         data: XLSX.utils.sheet_to_json(excelData.Sheets[name]),
@@ -17,6 +17,15 @@ const parse = () => {
   
     return parseExcelData[0].data;
 }
+// const parse = () => {
+//     const excelData = XLSX.readFile(path.join(__dirname, "../data/tienda360.xlsx"));
+//     const parseExcelData = Object.keys(excelData.Sheets).map((name) => ({
+//         name,
+//         data: XLSX.utils.sheet_to_json(excelData.Sheets[name]),
+//     }))
+  
+//     return parseExcelData[0].data;
+// }
 
 
 
