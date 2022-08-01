0d81aee367d79f6dff46172dd3923bb778824f70
GianMatty <matiasmauriciogiancarlo@gmail.com>
2022-08-01 13:52:28 -0500

    process.cwd() en ves de __dirname para rutear

--- src/controller/serverController.js
+++ src/controller/serverController.js
@@ -9,7 +9,7 @@ const XLSX = require("xlsx");
 
 
 const parse = () => {
-    const excelData = XLSX.readFile(path.join(process.cwd() + "/tienda360.xlsx"));
+    const excelData = XLSX.readFile(path.join(process.cwd() + "/src/data/tienda360.xlsx"));
     const parseExcelData = Object.keys(excelData.Sheets).map((name) => ({
         name,
         data: XLSX.utils.sheet_to_json(excelData.Sheets[name]),
