1b0e603e7c70fde6b55a508969eb0377f143f1bc
GianMatty <matiasmauriciogiancarlo@gmail.com>
2022-08-07 14:12:01 -0500

    updated

--- src/data/productos_resumen.js
+++ src/data/productos_resumen.js
@@ -1,58 +1,58 @@
-[DULCES, BEBIDAS, ASEO PERSONAL]
-[ Galletas, Chocolates, Dulces, Gaseosas, Mineral, Frugos, Jabones, Shampoos]
+// [DULCES, BEBIDAS, ASEO PERSONAL]
+// [ Galletas, Chocolates, Dulces, Gaseosas, Mineral, Frugos, Jabones, Shampoos]
 
 
-DULCES
-    GALLETAS
-    CHOCOLATES
-    DULCES
-    CEREALES
-    SNACKS
-BEBIDAS
-    GASEOSAS
-    MINERAL
-    FRUGOS
-    YOGURT
-    OTROS
-VERDURAS
-    VERDURAS
-    ESPECERIAS
-    CONDIMENTOS(maggy, canela etc)
-ASEO PERSONAL
-    JABONES
-    SHAMPOOS
-    CREMAS
-    PAPELES
-    AFEITADORES
-    TOALLAS HIGIENICAS
-    CREMA DENTAL
-    PAÑOS HUMEDOS
-    PAÑALES
-ASEO LIMPIEZA
-    ENFUEGUE
-    DETERGENTE
-    LEGIA
-ABARROTES
-    FIDEOS
-    ACEITES
-    LEGUMBRES
-    ARROZ
-    AZUCAR
-    ATUNES
-    CAFES(cafe,milo,etc)
-    COMIDA DE ANIMAL
-    BOLSAS Y PLASTICOS
-    OTROS (mantequilla, mermelada, etc)
-MEDICINAS
-    MEDICINA LOW
-    MEDICINA MEDIUM
-    MEDICINA HIEHT
-LIBRERIA
-    LIBRERIA
-UTILITARIOS
-    UTILITARIOS(corta uña encendedor,etc)
-ROPA
-    ROPA INTERIOR
-REGALOS
-    REGALOS
+// DULCES
+//     GALLETAS
+//     CHOCOLATES
+//     DULCES
+//     CEREALES
+//     SNACKS
+// BEBIDAS
+//     GASEOSAS
+//     MINERAL
+//     FRUGOS
+//     YOGURT
+//     OTROS
+// VERDURAS
+//     VERDURAS
+//     ESPECERIAS
+//     CONDIMENTOS(maggy, canela etc)
+// ASEO PERSONAL
+//     JABONES
+//     SHAMPOOS
+//     CREMAS
+//     PAPELES
+//     AFEITADORES
+//     TOALLAS HIGIENICAS
+//     CREMA DENTAL
+//     PAÑOS HUMEDOS
+//     PAÑALES
+// ASEO LIMPIEZA
+//     ENFUEGUE
+//     DETERGENTE
+//     LEGIA
+// ABARROTES
+//     FIDEOS
+//     ACEITES
+//     LEGUMBRES
+//     ARROZ
+//     AZUCAR
+//     ATUNES
+//     CAFES(cafe,milo,etc)
+//     COMIDA DE ANIMAL
+//     BOLSAS Y PLASTICOS
+//     OTROS (mantequilla, mermelada, etc)
+// MEDICINAS
+//     MEDICINA LOW
+//     MEDICINA MEDIUM
+//     MEDICINA HIEHT
+// LIBRERIA
+//     LIBRERIA
+// UTILITARIOS
+//     UTILITARIOS(corta uña encendedor,etc)
+// ROPA
+//     ROPA INTERIOR
+// REGALOS
+//     REGALOS
         
